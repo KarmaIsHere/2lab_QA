8460f55f63182d5c7e0d36ae0fd65fd76302169e
KarmaIsHere <letrixswitch@gmail.com>
2023-10-30 14:45:42 +0200

    code fix

--- src/main/java/org/deskify/service/UserService.java
+++ src/main/java/org/deskify/service/UserService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-import static org.deskify.utils.Validator.isValidEmail;
-
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -30,19 +28,7 @@ public class UserService {
                 .email(request.getEmail())
                 .build();
 
-        if (!validateUser(user)) {
-            throw new IllegalArgumentException("Username or email is already taken");
-        }
-        if (!isValidEmail(request.getEmail())){
-            throw new IllegalArgumentException("Incorrect email");
-        }
-
         return userRepository.save(user);
-    }
-
-    public boolean validateUser(User user) {
-        if (!fetchUsers(null, user.getUsername(), null,null,null).isEmpty()) return false;
-        return fetchUsers(null, null,null,null, user.getEmail()).isEmpty();
     }
 
     public List<User> fetchUsers(Long id, String username, String firstName, String lastName, String email) {
